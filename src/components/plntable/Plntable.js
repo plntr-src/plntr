b5ba1df7c00503b8ccb336912fcd844a2e13a070
ClarabelleCheng-Yue <cachengyue@gmail.com>
2019-05-03 01:14:38 -0700

    add more style

--- src/components/plntable/Plntable.js
+++ src/components/plntable/Plntable.js
@@ -5,23 +5,38 @@ import { categories } from '../constants';
 export const Plntable = props => {
 
   const putPlntsOnTable = (plnts) => {
-    return plnts.map( (plnt, i) => <td className="row" key={i} >{ /** plnt data */ }</td> )
+    const t = plnts.reduce((list, plnt) => {
+      let plntDeets = [];      
+      for (const k in plnt) {
+        let deet = plnt[k];
+        if (k === "companions" && deet.length === 0) {
+          deet = "--"
+        } else if (k === "hardiness" && deet.length > 0) {
+          deet = deet.join(", ");
+        } else if (k === "image") {
+          deet = <img className="plnt-img" src={ deet } />
+        }
+        plntDeets.push(<td>{ deet }</td>);
+      }
+
+      list.push(<tr>{ plntDeets }</tr>)
+      return list
+    }, [])
+    return t;
   }
+
+  console.log('plnts: ', props.data);
 
   return (
     <table className="plntable" >
-      <thead>
-        <tr>
-        { 
-          props.cols.map( (header, i) => <th key={i}>{ header }</th> ) 
-        }
-        </tr>
-      </thead>
-      <tbody>
-        {
-          putPlntsOnTable(props.data)
-        }
-      </tbody>
+      <tr>
+      { 
+        props.cols.map( (header, i) => <th key={i}>{ header }</th> ) 
+      }
+      </tr>
+      {
+        putPlntsOnTable(props.data)
+      }
     </table>  
   )
 }
